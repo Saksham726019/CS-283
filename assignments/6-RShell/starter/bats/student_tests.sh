86e3921db04db29dd65562764c3183f9b3f541e3
Saksham Rajbhandari <saksham726019@gmail.com>
2025-03-13 00:30:25 -0400

    Added tests for local mode

--- assignments/6-RShell/starter/bats/student_tests.sh
+++ assignments/6-RShell/starter/bats/student_tests.sh
@@ -4,11 +4,176 @@
 # 
 # Create your unit tests suit in this file
 
-@test "Example: check ls runs without errors" {
+@test "Local mode: check ls runs without errors" {
     run ./dsh <<EOF                
 ls
 EOF
-
     # Assertions
+    [ "$status" -eq 0 ]
+}
+
+@test "Local mode: Check pwd gives correct current working directory" {
+    run "./dsh" <<EOF                
+pwd
+EOF
+
+    stripped_output=$(echo "$output" | tr -d '[:space:]')
+
+    expected_output="$(pwd)localmodedsh4>dsh4>cmdloopreturned0"
+
+    echo "Captured stdout:" 
+    echo "Output: $output"
+    echo "Exit Status: $status"
+    echo "${stripped_output} -> ${expected_output}"
+
+    [ "$stripped_output" = "$expected_output" ]
+    [ "$status" -eq 0 ]
+}
+
+@test "Local mode: Piping with multiple spaces" {
+    run "./dsh" <<EOF
+ls     |    wc -l
+EOF
+
+    stripped_output=$(echo "$output" | tr -d '[:space:]')
+
+    expected_output="$(ls | wc -l)localmodedsh4>dsh4>cmdloopreturned0"
+
+    echo "Captured stdout:"
+    echo "Output: $output"
+    echo "Exit Status: $status"
+    echo "${stripped_output} -> ${expected_output}"
+
+    [ "$stripped_output" = "$expected_output" ]
+
+    [ "$status" -eq 0 ]
+}
+
+@test "Local mode: Echo quoted and unquoted arguments" {
+    run ./dsh <<EOF
+echo Test "Test Multi   word unquoted"   unquoted
+EOF
+
+    stripped_output=$(echo "$output" | tr -d '\t\n\r\f\v')
+
+    expected_output="Test Test Multi   word unquoted unquotedlocal modedsh4> dsh4> cmd loop returned 0"
+
+    echo "Captured stdout:"
+    echo "Output: $output"
+    echo "Exit Status: $status"
+    echo "${stripped_output} -> ${expected_output}"
+
+    [ "$stripped_output" = "$expected_output" ]
+    [ "$status" -eq 0 ]
+}
+
+@test "Local mode: tr to lowercase" {
+    run "./dsh" <<EOF
+echo "ABCdef" | tr 'A-Z' 'a-z'
+EOF
+
+    stripped_output=$(echo "$output" | tr -d '[:space:]')
+    expected_output="abcdeflocalmodedsh4>dsh4>cmdloopreturned0"
+
+    echo "Captured stdout:"
+    echo "Output: $output"
+    echo "Exit Status: $status"
+    echo "${stripped_output} -> ${expected_output}"
+
+    [ "$stripped_output" = "$expected_output" ]
+    [ "$status" -eq 0 ]
+}
+
+@test "Local mode: echo piped to cat" {
+    run "./dsh" <<EOF
+echo "Hello, Pipeline" | cat
+EOF
+
+    stripped_output=$(echo "$output" | tr -d '[:space:]')
+    expected_output="Hello,Pipelinelocalmodedsh4>dsh4>cmdloopreturned0"
+
+    echo "Captured stdout:"
+    echo "Output: $output"
+    echo "Exit Status: $status"
+    echo "${stripped_output} -> ${expected_output}"
+
+    [ "$stripped_output" = "$expected_output" ]
+    [ "$status" -eq 0 ]
+}
+
+@test "Local mode: Piping with no spaces between piped commands" {
+    run "./dsh" <<EOF
+ls|wc -l
+EOF
+
+    stripped_output=$(echo "$output" | tr -d '[:space:]')
+
+    expected_output="$(ls|wc -l)localmodedsh4>dsh4>cmdloopreturned0"
+
+    echo "Captured stdout:"
+    echo "Output: $output"
+    echo "Exit Status: $status"
+    echo "${stripped_output} -> ${expected_output}"
+
+    [ "$stripped_output" = "$expected_output" ]
+
+    [ "$status" -eq 0 ]
+}
+
+@test "Local mode: Piping with leading and trailing spaces" {
+    run "./dsh" <<EOF
+    ls   |   wc -l    
+EOF
+
+    stripped_output=$(echo "$output" | tr -d '[:space:]')
+
+    expected_output="$(ls   |   wc -l)localmodedsh4>dsh4>cmdloopreturned0"
+
+    echo "Captured stdout:"
+    echo "Output: $output"
+    echo "Exit Status: $status"
+    echo "${stripped_output} -> ${expected_output}"
+
+    [ "$stripped_output" = "$expected_output" ]
+
+    [ "$status" -eq 0 ]
+}
+
+@test "Local mode: cd to tmp" {
+    run "./dsh" <<EOF                
+cd /tmp
+pwd
+EOF
+
+    stripped_output=$(echo "$output" | tr -d '[:space:]')
+
+    expected_output="/tmplocalmodedsh4>dsh4>dsh4>cmdloopreturned0"
+
+    echo "Captured stdout:"
+    echo "Output: $output"
+    echo "Exit Status: $status"
+    echo "${stripped_output} -> ${expected_output}"
+
+    # Expected output should contain "/"
+    [ "$stripped_output" = "$expected_output" ]
+
+    [ "$status" -eq 0 ]
+}
+
+@test "Local mode: cd to non-existent path" {
+    run "./dsh" <<EOF
+cd /nonexistentpath
+EOF
+
+    stripped_output=$(echo "$output" | tr -d '[:space:]')
+
+    expected_output="localmodedsh4>error:commandfaileddsh4>cmdloopreturned0"
+
+    echo "Captured stdout:"
+    echo "Output: $output"
+    echo "Exit Status: $status"
+    echo "${stripped_output} -> ${expected_output}"
+    
+    [ "$stripped_output" = "$expected_output" ]
     [ "$status" -eq 0 ]
 }
